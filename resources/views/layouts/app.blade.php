0cbd3c8e69b09a555c1baf8ea10b29e042c83b5e
CuzImAzizx <100154535+CuzImAzizx@users.noreply.github.com>
2024-12-23 18:35:10 +0300

    Updated fonts

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,7 +9,8 @@
     integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
     integrity="sha384-..." crossorigin="anonymous">
-  <link rel="stylesheet" href="/phoneStyle.css">
+    <link href="https://fonts.googleapis.com/css2?family=Tajawal&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="/phoneStyle.css">
   <link rel="icon" type="image/x-icon" href="/icon.png">
 </head>
 
