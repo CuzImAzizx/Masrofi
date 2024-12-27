ec4854bbf83866dd8c896f2f79bf1a0663d08b91
CuzImAzizx <100154535+CuzImAzizx@users.noreply.github.com>
2024-12-27 15:45:42 +0300

    Added Schedule for updating daily requests

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,24 @@
 <?php
 
+use App\Models\Plan;
+use App\Models\Subscription;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote')->hourly();
+//Artisan::command('inspire', function () {
+//    $this->comment(Inspiring::quote());
+//})->purpose('Display an inspiring quote')->hourly();
+
+/**
+ * Refresh daily requests for all subscriptions
+ */
+Schedule::call(function () {
+    $subscriptions = Subscription::all();
+    foreach($subscriptions as $subscription){
+        $planId = $subscription->plan_id;
+        $plan = Plan::find($planId);
+        $subscription->daily_requests_left = $plan->daily_requests;
+        $subscription->update();
+    }
+})->daily();
